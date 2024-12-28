79b8b64affa079821da4236e1376f06b9e8effd6
Daniel Caiafa <1450558+dcaiafa@users.noreply.github.com>
2024-12-28 11:15:17 -0800

    day 18

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,7 +1,9 @@
 mod input;
 mod part1;
+mod part2;
 
 fn main() {
   let input = std::fs::read_to_string("input.txt").unwrap();
   println!("part1={}", part1::eval(&input, 71, 71, 1024).unwrap());
+  println!("part2={:?}", part2::eval(&input, 71, 71).unwrap());
 }
